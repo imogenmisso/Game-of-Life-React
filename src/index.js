4c2d5e0a7f034c6b126f1108be56293fee6fba33
Imogen Misso <imogenmisso@hotmail.co.uk>
2019-01-18 12:09:47 +0000

    Added grid component and updated index

--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Grid from './componenets/Grid'
+import Grid from './components/Grid'
 
 class Main extends React.Component {
 
   constructor() {
     super()
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
     this.state = {
-      generation: 0
+      generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
     }
+  }
+
+  selectBox = (row, col) => {
+    let gridCopy = arrayClone(this.state.gridFull)
+    gridCopy[row][col] = !gridCopy[row][col];
+    this.setState({
+      gridFull: gridCopy
+    })
   }
 
   render() {
     return(
       <div>
         <h1>The Game of Life</h1>
-        <Grid/>
+        <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
         <h2>Generations: {this.state.generation}</h2>
       </div>
     )
   }
+}
+
+function arrayClone(arr) {
+  return JSON.parse(JSON.stringify(arr));
 }
 
 
